ac060518f01b0f2dda290e3e894da16717a41f36
Alexey Gorbach <gorbach.alexey@gmail.com>
2017-03-28 17:47:46 +0300

    remove unused namespaces

--- AdobeSign.Net/RestAPI.cs
+++ AdobeSign.Net/RestAPI.cs
@@ -1,15 +1,11 @@
 ﻿
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AdobeSignNet
